832ce070066f76e49e59a6558b0a225463a23844
Saiya-Dev <edelsonjaelta@gmail.com>
2024-02-01 15:50:35 -0400

    comenze el login

---
+++ frontend/src/home/page/login/components/login.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import { FaFacebook, FaInstagram, FaGoogle } from "react-icons/fa";
+// import { BiCoffeeTogo } from "react-icons/bi";
+import "../css/login.css";
+import axios from "axios";
+
+const Login = () => {
+  //? creating the usestate
+  const [value, stateValues] = useState({
+    user: "",
+    clave: "",
+  });
+  //? creating the usenavigate
+  const navigate = useNavigate();
+
+  //? this is the handleSumit or manager of menssage.
+  const handleSumit = (e) => {
+    e.preventDefault();
+    console.log(value);
+    axios
+      .post("http://localhost:3000/login", value)
+      .then((res) => {
+        if (value.clave != "" && value.user != "") {
+          if (res.data.Status === "Success") {
+            navigate("/table");
+          } else {
+            navigate("/login");
+            alert("Contraseña o usuario incorrectos");
+          }
+        } else {
+          alert("rellene todos los campos");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <div className="container">
+        <form action="" onSubmit={handleSumit}>
+          <div className="input-group">
+            <span className="icon">
+              {/* <BiCoffeeTogo /> */}
+              <span className="logo">Json Coffe</span>
+            </span>
+
+            <h1>Login in the system</h1>
+
+            <div className="other-logins">
+              <a className="google" href="#">
+                {/* <FaGoogle /> */}
+                <span className="logo-span">Continue with Google</span>
+              </a>
+              <a className="instagram" href="#">
+                {/* <FaInstagram /> */}
+                <span className="logo-span">Continue with Instagram</span>
+              </a>
+              <a className="facebook" href="#">
+                {/* <FaFacebook /> */}
+                <span className="logo-span">Continue with Facebook</span>
+              </a>
+            </div>
+            <hr />
+            <div className="or">
+              <span>or</span>
+            </div>
+
+            <div className="input-feild">
+              <input
+                type="text"
+                name="username"
+                onChange={(e) =>
+                  stateValues({ ...value, user: e.target.value })
+                }
+              />
+              <label htmlFor="">username</label>
+            </div>
+
+            <div className="input-feild">
+              <input
+                type="password"
+                name="password"
+                onChange={(e) =>
+                  stateValues({ ...value, clave: e.target.value })
+                }
+              />
+              <label htmlFor="">Password</label>
+            </div>
+
+            <div className="button">
+              <button>Login</button>
+            </div>
+
+            <div className="link-router">
+            <Link  to="/register">
+                Create Account
+              </Link>
+              <Link  to="/forget">
+                forget you password 
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- frontend/src/router/App.jsx
+++ frontend/src/router/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
-import Login from "../home/page/login/login";
+import Login from "../home/page/login/components/login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Login />} />
+        <Route exact path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
