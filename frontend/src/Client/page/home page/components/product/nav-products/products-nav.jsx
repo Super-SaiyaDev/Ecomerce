777b6d2ddf07f56ac0033cff50c44f39fee08e5a
Edelsom <edelsonjaelta@gmail.com>
2024-03-13 14:21:08 -0400

    para rinaldi pueda trabajar conmigo

---
+++ frontend/src/Client/page/home page/components/product/nav-products/products-nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ProductsNav = () => {
+  return (
+    <>
+      <div className="container-products-nav">
+        <div className="container-swiper">
+          <div className="container-icon"></div>
+        </div>
+        <div className="filtros"></div>
+        <div className="prc-impuestos"></div>
+      </div>
+    </>
+  );
+};
+
+export default ProductsnNav;
