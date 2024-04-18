a9495fe15cd0ba05f6e37744923a96816617d825
Rinaldi_Bueno <buenosantiagorinaldi@gmail.com>
2024-04-18 11:07:23 -0400

    Arregle la animacion de la homepage, estoy trabajando en nav del sistema

--- frontend/src/Client/page/home page/home-page.jsx
+++ frontend/src/Client/page/home page/home-page.jsx
@@ -6,6 +6,7 @@ import Products from "./components/product/products";
 import Footer from "./components/footer/footer";
 import "./css/home-page.css";
 import { useSpring, animated } from "react-spring";
+import {motion} from 'framer-motion'
 
 const PageHome = () => {
   const [style, set] = useSpring(() => ({
@@ -20,13 +21,15 @@ const PageHome = () => {
           <Navbar></Navbar>
           <Navbar2></Navbar2>
         </nav>
-        <animated.div
-          style={style}
-          onMouseEnter={() => set({ scale: 1.1, backgroundColor: "#f00" })}
-          onMouseLeave={() => set({ scale: 1, backgroundColor: "#fff" })}
+       
+     <motion.div
+          initial={{ opacity: 0, scale: 0.8 }} // Comienza transparente y un 80% de su tamaño original
+          animate={{ opacity: 1, scale: 1 }} // Termina completamente opaco y a su tamaño original
+          transition={{ duration: 1 }} // La transición dura 1 segundo
         >
           <Header></Header>
-        </animated.div>
+
+        </motion.div>
       </header>
       <section>
         <Products></Products>
--- frontend/src/system/dashboard/components/navbar/navabar.jsx
+++ frontend/src/system/dashboard/components/navbar/navabar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../css/navbarSystem.css";
 import { FaWhatsapp } from "react-icons/fa6";
-import { FaInstagram, FaUserCog } from "react-icons/fa";
+import { FaInstagram, FaUserCog,  FaUser } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
 import { BsFillHouseHeartFill } from "react-icons/bs";
 const Navbar2 = () => {
@@ -18,92 +18,28 @@ const Navbar2 = () => {
           </div>
 
           <div className="input-groupt-nav">
-            <div className="input-feild-nav">
-              <select name="op-user" className="nav-select">
-                <option value="op1" className="op-user1">
-                  Contactos
-                </option>
-                <option value="op2" className="op-user2">
-
-                </option>
-                <option value="op3" className="op-user3">
-                  Ayuda
-                </option>
-                <option value="op4" className="op-user4">
-                  Configuración
-                </option>
-              </select>
-            </div>
-
-            <div className="input-feild-nav">
-              <select name="op-user" id="select">
-                <option value="op1" className="op-user1">
-                  Mensajes
-                </option>
-                <option value="op2" className="op-user2">
-                  hola mundo
-                </option>
-                <option value="op3" className="op-user3">
-                  hola mundo
-                </option>
-                <option value="op4" className="op-user4">
-                  hola mundo
-                </option>
-              </select>
-            </div>
-
-            <div className="input-feild-nav">
-              <select name="op-user" id="select">
-                <option value="op1" className="op-user1">
-                Ayuda
-                </option>
-                <option value="op2" className="op-user2">
-                  hola mundo
-                </option>
-                <option value="op3" className="op-user3">
-                  hola mundo
-                </option>
-                <option value="op4" className="op-user4">
-                  hola mundo
-                </option>
-              </select>
-            </div>
-
-            <div className="btn-contact-nav">
-              <button className="btn-nav">logout</button>
-            </div>
+            <input type="text" className="input-nav-search" placeholder="search"/>    
+            <CiSearch className="buscar"/>    
           </div>
         </div>
 
         <div className="conteiner-nav-left">
 
           <div className="input-felid-nav">
-            <input type="text" className="input-nav-search" />
+
             <span className="nav-span-search">
-              <CiSearch className="nav-icon-search" />
             </span>
+            
           </div>
 
-          <div className="nv-icons-system">
-            <i>
-              <FaWhatsapp className="icon-menssage icon-nav" />
-            </i>
-
-            <i>
-              <FaInstagram className="icon-notification icon-nav" />
-            </i>
-
-            <i>
-              <CiSearch className="icon-alert icon-nav" />
-            </i>
-          </div>
+          
           <div className="profile-user">
-            <Link>
-              <i>
-                <FaUserCog />
+            <Link className="ico">
+              <i >
+                <FaUser/>
               </i>
+
             </Link>
-            <label className="title-user" htmlFor="">Nombre del usuario </label>
           </div>
         </div>
       </nav>
