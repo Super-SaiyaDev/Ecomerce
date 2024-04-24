32335aaad33dd5a494e4faa67616ae1a03993ae5
Edelsom <edelsonjaelta@gmail.com>
2024-04-24 08:13:57 -0400

    nuevo cambio

--- frontend/src/system/dashboard/components/sliderbar/sliderbar.jsx
+++ frontend/src/system/dashboard/components/sliderbar/sliderbar.jsx
@@ -38,7 +38,7 @@ function Sliderbar() {
             className={activeMenu === "usuario" ? "active" : ""}
             onClick={() => handleMenuClick("usuario")}
           >
-            <Link className="lnk-sidebar" to="Reportes">
+            <Link className="lnk-sidebar  " to="/table">
               Usuario
             </Link>
           </li>
