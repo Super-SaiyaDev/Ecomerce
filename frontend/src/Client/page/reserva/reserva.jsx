7b0fe0dcf76279f1c5e74f7c7f72cd705e4d6476
rbueno25 <buenosantiagorinaldi@gmail.com>
2024-05-02 15:57:04 -0400

    se le cambiaron algunos colores al area de reserva

--- frontend/src/Client/page/reserva/reserva.jsx
+++ frontend/src/Client/page/reserva/reserva.jsx
@@ -3,6 +3,7 @@ import CuadroCompra from './components/cuadro-compr/CuadroCompra'
 import Primero from './components/main1/Primero'
 import HeroReserva from './components/hero/heroreserva'
 import NavReserva from './components/navbar/nav'
+import Navbar from '../home page/components/navbar/menu-1'
 import './reserva.css'
 
 const Reserva = () => {
@@ -12,6 +13,7 @@ const Reserva = () => {
     <div className="bod33">
           <header>
       <nav>
+        <Navbar/>
         <NavReserva />
       </nav>
       <HeroReserva />
