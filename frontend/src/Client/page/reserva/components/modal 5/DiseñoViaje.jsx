793d040750de7db5b702dda45613d148a0857ac7
LuisCal14 <luiscal1402@gmail.com>
2024-05-16 12:39:12 -0400

    voy actulizr la rama mein

--- frontend/src/Client/page/reserva/components/modal 5/DiseñoViaje.jsx
+++ frontend/src/Client/page/reserva/components/modal 5/DiseñoViaje.jsx
@@ -3,7 +3,15 @@ import './diseñoviaje.css'
 
 function DiseñoViaje() {
   return (
-    <div>DiseñoViaje</div>
+    <div>
+      <div className="top">
+        <h1>Diseña tu viaje</h1>
+        <div className="separ"></div>
+      </div>
+      <div className="cuerpoo">
+        <label htmlFor="correo"></label>
+      </div>
+    </div>
   )
 }
 
